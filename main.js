c32ea59d741d1952f957859541d2f6528ec0bf5c
xhbalta <anay.dcho@gmail.com>
2026-03-15 12:38:23 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // main.js - Router principal con soporte completo para búsqueda e integración con sidebar y reproductor
 import { DATA, renderFeed, renderGrid, renderEpisodio, renderSerie, renderCategoryPills } from './show.js';
-import { getEpisodioByDetailUrl, getSerieByUrl, getAllEpisodios } from './episodios.js';
+import { getEpisodioByDetailUrl, getSerieByUrl, getAllEpisodios } from 'https://example.baltaanay.org./episodios.js';
 import './player.js';
 
 // Páginas especiales
